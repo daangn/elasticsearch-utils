9e2b53553b2be957340ec87f9e6477cecd4719bd
Ed Jeon <muikor@gmail.com>
2017-01-31 18:34:30 +0900

    Update script

--- restart_if_updated.sh
+++ restart_if_updated.sh
@@ -5,8 +5,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DOWNLOADED=$( $DIR/download.rb )
 
 if [ -z $DOWNLOADED ]; then
-  cd /opt/mecab-ko-dic-2.0.1-20150920 && \
-    tools/add-userdic.sh && \
-    make install
-  $DIR/restart.rb
+  echo "Not updated"
+  exit 0
 fi
+
+cd /opt/mecab-ko-dic-2.0.1-20150920 && \
+  tools/add-userdic.sh && \
+  make install
+$DIR/restart.rb
