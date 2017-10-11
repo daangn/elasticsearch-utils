a5450daf3f8cac7f100be2b63c6a3696e609fdd6
Ed Jeon <muikor@gmail.com>
2017-10-11 17:14:00 +0900

    Replace all custom word priority

--- replace.sh
+++ replace.sh
@@ -2,10 +2,4 @@
 # 가중치를 임의로 조정
 # https://groups.google.com/forum/#!searchin/eunjeon/%EA%B8%B0%EB%B6%84%EC%84%9D%7Csort:relevance/eunjeon/aOVOlvxbSs0/jNgNwNGb7w0J
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-while read line; do
-  if [[ ! -z $line ]]; then
-    sed -i -E "s/^(${line},[0-9]+,[0-9]+),([0-9]+)/\1,500/" $1
-  fi
-done <"${DIR}/high_priority_words.txt"
+sed -i -E "s/^([^,]+,[0-9]+,[0-9]+),([0-9]+)/\1,500/" $1
