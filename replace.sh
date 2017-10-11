354f4eb9233b62a7a41d40a647f187d5a515dc2a
Ed Jeon <muikor@gmail.com>
2017-10-11 12:38:57 +0900

    우선순위 높이는 단어파일 추가

--- replace.sh
+++ replace.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 # 가중치를 임의로 조정
 # https://groups.google.com/forum/#!searchin/eunjeon/%EA%B8%B0%EB%B6%84%EC%84%9D%7Csort:relevance/eunjeon/aOVOlvxbSs0/jNgNwNGb7w0J
-sed -i -E 's/^(가방,[0-9]+,[0-9]+),([0-9]+)/\1,500/' $1
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+while read line; do
+  if [[ ! -z $line ]]; then
+    sed -i -E "s/^(${line},[0-9]+,[0-9]+),([0-9]+)/\1,500/" $1
+  fi
+done <"${DIR}/high_priority_words.txt"
